c0b164f8c023426432a7925d36d9dcde7f611b0d
Rodrigo Santos da Silva <45763521+Rodriigosantos01@users.noreply.github.com>
2019-11-12 22:22:36 -0300

    Add files via upload
    Alteração do banco de dados, permitir id_usuarios como null para o sistema desktop

--- src/java/utils/agenda.sql
+++ src/java/utils/agenda.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 07-Nov-2019 às 05:41
--- Versão do servidor: 5.7.14
--- PHP Version: 5.6.25
+-- Tempo de geração: 13-Nov-2019 às 02:21
+-- Versão do servidor: 10.4.8-MariaDB
+-- versão do PHP: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `agenda`
+-- Banco de dados: `agenda`
 --
 
 -- --------------------------------------------------------
@@ -28,6 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `pessoa` (
   `id` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
   `nome` varchar(50) NOT NULL,
   `telefone` int(11) NOT NULL,
   `cep` int(11) NOT NULL,
@@ -35,11 +38,17 @@ CREATE TABLE `pessoa` (
   `bairro` varchar(25) NOT NULL,
   `cidade` varchar(25) NOT NULL,
   `estado` varchar(2) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `id_usuario` int(11) NOT NULL
+  `email` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---------------------------------------------------------
+--
+-- Extraindo dados da tabela `pessoa`
+--
+
+INSERT INTO `pessoa` (`id`, `id_usuario`, `nome`, `telefone`, `cep`, `logradouro`, `bairro`, `cidade`, `estado`, `email`) VALUES
+(36, 23, 'Rodrigo Santos', 954596565, 0, 'Rua tal', 'Barra Funda', 'São Paulo', 'SP', 'Rodrigo@gmail.com');
+
+-- --------------------------------------------------------
 
 --
 -- Estrutura da tabela `usuarios`
@@ -52,35 +61,47 @@ CREATE TABLE `usuarios` (
   `senha` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Indexes for dumped tables
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
+(23, 'Rodrigo Santos', 'Rodrigo@gmail.com', '123'),
+(26, 'Celina', 'celina@gmail.com', '123');
+
+--
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `pessoa`
+-- Índices para tabela `pessoa`
 --
 ALTER TABLE `pessoa`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `usuarios`
+-- Índices para tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `pessoa`
+-- AUTO_INCREMENT de tabela `pessoa`
 --
 ALTER TABLE `pessoa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
 --
--- AUTO_INCREMENT for table `usuarios`
+-- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
